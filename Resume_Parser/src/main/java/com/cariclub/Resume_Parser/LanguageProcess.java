09b256f75b269d502adb1b21342ae4599d2dcfb3
Mohit <mohit@Mohits-MacBook-Pro.local>
2016-06-24 18:01:48 -0400

    Default Annotations working

--- Resume_Parser/src/main/java/com/cariclub/Resume_Parser/LanguageProcess.java
+++ Resume_Parser/src/main/java/com/cariclub/Resume_Parser/LanguageProcess.java
@@ -4,8 +4,11 @@ import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 
+import gate.Annotation;
 import gate.AnnotationSet;
 import gate.Corpus;
 import gate.CorpusController;
@@ -13,10 +16,14 @@ import gate.Document;
 import gate.Factory;
 import gate.FeatureMap;
 import gate.Gate;
+import gate.GateConstants;
+import gate.LanguageAnalyser;
 import gate.creole.ResourceInstantiationException;
+import gate.creole.SerialAnalyserController;
 import gate.util.GateException;
 import gate.util.Out;
 import gate.util.persistence.PersistenceManager;
+import static gate.Utils.*;
 
 public class LanguageProcess {
 
@@ -55,35 +62,90 @@ public class LanguageProcess {
 	}
 	
 	public void processDocument(File file) throws MalformedURLException, GateException{
-		// create a GATE corpus and add a document for each command-line argument
-	    Corpus corpus = Factory.newCorpus("StandAloneAnnie corpus");
-	    URL u = file.toURI().toURL();
-	    FeatureMap params = Factory.newFeatureMap();
-	    params.put("sourceUrl", u);
-	    params.put("preserveOriginalContent", new Boolean(true));
-	    params.put("collectRepositioningInfo", new Boolean(true));
-	    Out.prln("Creating doc for " + u);
-	    Document doc = (Document) Factory.createResource("gate.corpora.DocumentImpl", params);
-	    corpus.add(doc);
+	// load ANNIE plugin - you must do this before you can create tokeniser
+	// or JAPE transducer resources.
+	Gate.getCreoleRegister().registerDirectories(
+	new File(Gate.getPluginsHome(), "ANNIE").toURI().toURL());
+
+	 // Build the pipeline
+	SerialAnalyserController pipeline = (SerialAnalyserController)Factory.createResource("gate.creole.SerialAnalyserController");
+	LanguageAnalyser tokeniser = (LanguageAnalyser)Factory.createResource("gate.creole.tokeniser.DefaultTokeniser");
+	//LanguageAnalyser jape = (LanguageAnalyser)Factory.createResource("gate.creole.Transducer", gate.Utils.featureMap("grammarURL", new     
+	//File("SportsCategory.jape").toURI().toURL(),"encoding", "UTF-8")); // ensure this matches the file
+	pipeline.add(tokeniser);
+	//pipeline.add(jape);
+		
+		
+	// create a GATE corpus and add a document for each command-line argument
+	 Corpus corpus = Factory.newCorpus("StandAloneAnnie corpus");
+	 URL u = file.toURI().toURL();
+	 FeatureMap params = Factory.newFeatureMap();
+	 params.put("sourceUrl", u);
+	 params.put("preserveOriginalContent", new Boolean(true));
+	 params.put("collectRepositioningInfo", new Boolean(true));
+	 Out.prln("Creating doc for " + u);
+	 Document doc = (Document) Factory.createResource("gate.corpora.DocumentImpl", params);
+	 corpus.add(doc);
 	    
 	 // tell the pipeline about the corpus and run it
-	    setCorpus(corpus);
-	    annieExecute();
-	   
+	 pipeline.setCorpus(corpus);
+	 setCorpus(corpus);
+	 annieExecute();
+	 pipeline.execute();
+	    
+	 System.out.println("Found annotations of the following types: " +
+	 doc.getAnnotations().getAllTypes());
+	 
 	 // for the document, get an XML document with the
 	 // person and location names added   
-	    Iterator iter = corpus.iterator();
-	    String startTagPart_1 = "<span GateID=\"";
-	    String startTagPart_2 = "\" title=\"";
-	    String startTagPart_3 = "\" style=\"background:Red;\">";
-	    String endTag = "</span>";
-	    if(iter.hasNext()){
+	 Iterator iter = corpus.iterator();
+	 if(iter.hasNext()){
 	    	Document doc1 = (Document) iter.next();
 	    	AnnotationSet defaultAnnotSet = doc1.getAnnotations();
+	    	AnnotationSet curAnnSet;
+	    	Annotation currAnnot;		
+	    
+	    	//Get Name
+				curAnnSet = defaultAnnotSet.get("Person");
+				if(curAnnSet.iterator().hasNext() ){
+					currAnnot = (Annotation) curAnnSet.iterator().next();
+					System.out.println("Name " + stringFor(doc1, currAnnot));
+					}
 	    	
-	    	
-	    }
-  }
+	    	// Get Email
+				curAnnSet = defaultAnnotSet.get("Address");
+				if(curAnnSet.iterator().hasNext() && curAnnSet.iterator().next().getFeatures().get("kind").equals("email")){
+					currAnnot = (Annotation) curAnnSet.iterator().next();
+					System.out.println("Email " + stringFor(doc1, currAnnot));
+					}
+				
+				//Location
+				curAnnSet = defaultAnnotSet.get("Location");
+			//getAllInAnnSet(curAnnSet,doc1);
+				if(curAnnSet.iterator().hasNext() ){
+					currAnnot = (Annotation) curAnnSet.iterator().next();
+					System.out.println("Location " + stringFor(doc1, currAnnot));
+					}
+				
+				curAnnSet = defaultAnnotSet.get("ProfileSection");
+				//getAllInAnnSet(curAnnSet,doc1);
+					if(curAnnSet.iterator().hasNext() ){
+						currAnnot = (Annotation) curAnnSet.iterator().next();
+						System.out.println("Location " + stringFor(doc1, currAnnot));
+						}
+				
+				
+			}
+	  }
+  
+	
+	private void getAllInAnnSet(AnnotationSet as, Document doc){
+		for (Annotation a : as){
+			System.out.println(a );
+			System.out.println(stringFor(doc,a));
+		}
+		
+	}
 	
 	public static void main(String args[]) throws GateException{
 		//loadGateAndAnnie();
--- Resume_Parser/src/main/java/com/cariclub/Resume_Parser/PdfDocument.java
+++ Resume_Parser/src/main/java/com/cariclub/Resume_Parser/PdfDocument.java
@@ -2,6 +2,7 @@ package com.cariclub.Resume_Parser;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import org.apache.tika.exception.TikaException;
@@ -17,11 +18,16 @@ public class PdfDocument extends Doc {
 
 	@Override
 	public File parser(Doc doc) throws IOException, SAXException, TikaException  {
-		File file = new File("ParsedResume.txt");
 		PDFParser pdfParser = new PDFParser();
 		pdfParser.parse(doc.getFileInput(),doc.getBodyContentHandler(),doc.getMetadata(),doc.getParseContext());
-		System.out.println("Contents of the PDF :" + doc.getBodyContentHandler().toString());
-		return null;
+		FileWriter fw = new FileWriter("ParsedResume.txt");
+		fw.write(doc.getBodyContentHandler().toString());
+		fw.flush();
+		fw.close();
+	
+		File file = new File("ParsedResume.txt");
+	//	File file = new File("ex2.txt");
+		return file;
 	
 	}
 }
